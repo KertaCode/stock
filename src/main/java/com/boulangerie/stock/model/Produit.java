f88281812685d6c217d214274cbe7af0d23b1752
KertaCode <kertamengi@gmail.com>
2026-06-27 11:09:30 +0100

    Ajouter table produit

---
+++ src/main/java/com/boulangerie/stock/model/Produit.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.boulangerie.stock.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author Josephine Mayaya
+ */
+@Entity
+@Table(name = "Produit")
+@lombok.AllArgsConstructor
+@lombok.Getter
+@lombok.Setter
+public class Produit {
+    @Id
+    @Column(name = "code", length = 20, nullable = false)
+    private String code;
+    
+    @Column(name = "libelle", length = 200)
+    private String libelle;
+    
+    @ManyToOne
+    @JoinColumn(name = "idType", referencedColumnName = "idType")
+    private TypeProduit typeProduit;
+    
+    @Column(name = "qteStock")
+    private BigInteger qteStock;
+    
+    @Column(name = "prixUnit")
+    private BigInteger prixUnit;
+    
+    @Column(name = "pourcRist")
+    private Double pourcRist;
+    
+    @Column(name = "unitMesure")
+    private UniteMesure unitMesure;
+    
+    @Column(name = "createdAt",columnDefinition = "DATETIME", nullable = false)
+    private LocalDateTime createdAt;
+    
+    @PrePersist
+    public void Prepersit(){
+        if(createdAt == null){
+            createdAt = LocalDateTime.now();
+        }
+    }
+    
+}
