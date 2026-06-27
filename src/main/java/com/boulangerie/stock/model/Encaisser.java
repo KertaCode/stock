c2b376063b9fd73a36166d4af41f731d8916faf9
Faustin Flory <pdgfaustin@gmail.com>
2026-06-27 11:51:05 +0100

    Encaisser commenté

--- src/main/java/com/boulangerie/stock/model/Encaisser.java
+++ src/main/java/com/boulangerie/stock/model/Encaisser.java
@@ -14,6 +14,8 @@ import java.time.LocalDate;
 @lombok.Setter
 public class Encaisser {
     @Id
+    //Génération automatique de la valeur de la clé
+    //Condition : le type doit être un numérique entier
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "idEncaisser")
     private Long idEncaisser;
@@ -21,7 +23,10 @@ public class Encaisser {
     private LocalDate dateEncaisser;
     @Column(name = "montantEncaisser")
     private Long montantEncaisser;
+    //Création de la clé étrangère : relation ManyToOne
     @ManyToOne
+    //Spécification de la colonne de liaison
+    //Elle doit être une clé primaire dans la table d'origine
     @JoinColumn(name = "code")
     private Produit produit;
     @ManyToOne
